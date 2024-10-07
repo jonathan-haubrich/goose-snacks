26d8bd0a909ed25d23db95f26bd8e2877c606ff8
jonathan.haubrich <spacecowboy4@gmail.com>
2024-10-06 23:22:20 -0400

    Remove console logs

--- src/lib/clientManager.ts
+++ src/lib/clientManager.ts
@@ -8,7 +8,6 @@ if(!(global as any).clients) {
 // Function to add new WebSocket clients
 export function addClient(client: WebSocket) {
     (global as any).clients.add(client);
-  console.log("Adding client: " + (global as any).clients.size);
 
   client.on('close', () => {
     (global as any).clients.delete(client);
@@ -17,9 +16,7 @@ export function addClient(client: WebSocket) {
 
 // Function to broadcast vote updates to all WebSocket clients
 export function broadcastVotesUpdate(productId: number) {
-  console.log("Broadcasting votes!");
   (global as any).clients.forEach((client: WebSocket) => {
-    console.log("Client: " + client);
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify({ productId }));
     }
